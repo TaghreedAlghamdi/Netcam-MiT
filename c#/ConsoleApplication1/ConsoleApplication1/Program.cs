dd16ac7416f53aaa549db40a00c6507004f694a0
Rizwan Merchant <kommandantdrake@gmail.com>
2016-11-03 10:37:58 -0500

    First commit found online c# code to communicate to netcam using HTTP with SOAP Objects. Need to rewrite code into python and find protocol list for netcam device. Link to original code: http://community.wemothat.com/t5/WEMO-Ideas/WeMo-SDK-for-Java-Application-or-C-C/idi-p/29904

---
+++ c#/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace WeMoSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string IPADDR = "10.68.68.101";
+            string PORT = "49153";
+            string TARGETSTATUS = "0";
+
+            HttpWebRequest req = WebRequest.Create("http://" + IPADDR + ":" + PORT + "/upnp/control/basicevent1") as HttpWebRequest;
+            string reqContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+            reqContent += "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">";
+            reqContent += "<s:Body>";
+            reqContent += "<u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\">";
+            reqContent += "<BinaryState>"+TARGETSTATUS+"</BinaryState>";
+            reqContent += "</u:SetBinaryState>";
+            reqContent += "</s:Body>";
+            reqContent += "</s:Envelope>";
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            req.ContentType = "text/xml; charset=\"utf-8\"";
+            req.Headers.Add("SOAPACTION:\"urn:Belkin:service:basicevent:1#SetBinaryState\"");
+            req.Method = "POST";
+
+            using (Stream requestStream = req.GetRequestStream())
+            {
+                requestStream.Write(encoding.GetBytes(reqContent), 0,
+                    encoding.GetByteCount(reqContent));
+            }
+
+            try
+            {
+                HttpWebResponse response = req.GetResponse() as HttpWebResponse;
+                using (Stream rspStm = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(rspStm))
+                    {
+                        Console.WriteLine("Response Description: " + response.StatusDescription);
+                        Console.WriteLine("Response Status Code: " + response.StatusCode);
+                        Console.WriteLine("responseBody: " + reader.ReadToEnd());
+                    }
+                }
+                Console.WriteLine("Success: " + response.StatusCode.ToString());
+            }
+            catch (System.Net.WebException ex)
+            {
+                Console.WriteLine("Exception message: " + ex.Message);
+                Console.WriteLine("Response Status Code: " + ex.Status);
+                
+            }
+            while (!Console.KeyAvailable) System.Threading.Thread.Sleep(500);
+
+
+
+        }
+    }
+}
